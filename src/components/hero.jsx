56c7e7fa27408cd54479c314a644096630c6f1fa
Dave Poon <davepoon@davepoon.net>
2016-02-06 17:38:31 +1100

    file added.

---
+++ src/components/hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Hero extends React.Component {
+
+
+  render() {
+    return(
+      <section className="hero">
+        <h1>React</h1>
+        <h2>A javascript library for building user interfaces</h2>
+        <a className="btn btn-info" href="https://github.com/davepoon/react-gulp-starter-kit">Download React Gulp Starter Kit</a>
+        <h3>Getting everything you need for React using React Gulp Starter Kit</h3>
+      </section>
+    )
+  }
+}
